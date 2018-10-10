5d134300061cbd8f9162a3ec8c7d8aa638e77302
kylerisse <kylerisse@users.noreply.github.com>
2018-10-09 23:17:44 -0700

    simplify presentations import

--- client/schedule.js
+++ client/schedule.js
@@ -6,25 +6,27 @@ var presentationList = []
 var eventList = []
 var scheduleReady = false
 
+/* exported Presentation */
 class Presentation {
   constructor() {
-    this.name
-    this.description
-    this.location
-    this.startTime
-    this.endTime
-    this.speakers = []
-    this.topic
+    this.Name
+    this.Description
+    this.Location
+    this.StartTime
+    this.EndTime
+    this.Speakers = []
+    this.Topic
   }
 }
 
+/* exported Event */
 class Event {
   constructor() {
-    this.name
-    this.description
-    this.location
-    this.startTime
-    this.endTime
+    this.Name
+    this.Description
+    this.Location
+    this.StartTime
+    this.EndTime
   }
 }
 
@@ -34,33 +36,19 @@ function initSchedule() {
 }
 
 function populateSchedule(blob) {
-  if (blob.Presentations.length !== presentationList.length) {
-    scheduleReady = false
-    presentationList = []
-    for (let i = 0; i < blob.Presentations.length; i++) {
-      let p = new Presentation()
-      p.name = blob.Presentations[i].Name
-      p.description = blob.Presentations[i].Description
-      p.location = blob.Presentations[i].Location
-      p.startTime = blob.Presentations[i].StartTime
-      p.endTime = blob.Presentations[i].EndTime
-      p.speakers = blob.Presentations[i].Speakers
-      p.topic = blob.Presentations[i].Topic
-      presentationList.push(p)
-    }
-  }
-  if (blob.Events.length !== eventList.length) {
-    scheduleReady = false
-    eventList = []
-    for (let i = 0; i < blob.Events.length; i++) {
-      let e = new Event()
-      e.name = blob.Events[i].Name
-      e.description = blob.Events[i].Description
-      e.location = blob.Events[i].Location
-      e.startTime = blob.Events[i].StartTime
-      e.endTime = blob.Events[i].EndTime
-      eventList.push(e)
-    }
+  scheduleReady = false
+  presentationList = []
+  for (let i = 0; i < blob.Presentations.length; i++) {
+    let item = blob.Presentations[i]
+    let p = new Presentation()
+    p.Name = item.Name
+    p.Description = item.Description
+    p.Location = item.Location
+    p.StartTime = new Date(item.StartTime)
+    p.EndTime = new Date(item.EndTime)
+    p.Speakers = item.Speakers
+    p.Topic = item.Topic
+    presentationList.push(p)
   }
   scheduleReady = true
 }
